55ccc28362dd75da6aed665355c34ddb89bffad2
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-07-23 23:49:58 +0300

    fib

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -2,8 +2,10 @@ def fibonacci_sequence(n):
     if n <= 0:
         return []
 
-    fib_numbers = [0, 1]
-    for i in range(2, n + 1):
-        fib_numbers.append(fib_numbers[i - 1] + fib_numbers[i - 2])
+    fibonacci_numbers = [0, 1]
 
-    return fib_numbers
+    for i in range(2, n):
+        next_number = fibonacci_numbers[-1] + fibonacci_numbers[-2]
+        fibonacci_numbers.append(next_number)
+
+    return fibonacci_numbers[:n]
