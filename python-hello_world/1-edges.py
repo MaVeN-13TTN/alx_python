0133f8162fc5022f7a93359d60c32b6f6194d327
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-07-24 00:23:21 +0300

    1

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,3 +1,4 @@
+'''
 word = "Holberton"
 word_first_3 = word [:3]
 word_last_2 = word [7:]
@@ -5,3 +6,8 @@ middle_word = word [-8:-1]
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
+'''
+word = ["school", "Holberton"]
+for word in word: 
+    word_first_3, word_last_2, middle_word = word[:3], word[-2:], word[1:-1]; 
+print(f"First 3 letters: {word_first_3}\nLast 2 letters: {word_last_2}\nMiddle word: {middle_word}\n")
