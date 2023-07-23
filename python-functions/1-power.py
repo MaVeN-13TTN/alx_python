88c6e32470e54fdcf734c0f95522103369af3ffc
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-07-23 23:31:38 +0300

    a^b

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,8 +1,7 @@
 def pow(a, b):
     return a ** b
-#pow = __import__('1-power').pow
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
+pow(2, 2)
+pow(98, 2)
+pow(98, 0)
+pow(100, -2)
+pow(-4, 5)
