eb50e19a20f951121e6bd891d39c50cc8c5292d0
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-07-27 22:41:45 +0300

    err msg

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,6 +1,3 @@
-
-def raise_exception_msg(message=""):
-    error_message = f"NameError: {message}"
-    exec("raise " + error_message)
-    #def raise_exception_msg_with_line_number(message="", line_number=0):
-        #error_message = f"NameError: {message} at line {line_number}"
+def raise_exception_msg(msg):
+    """Raises a NameError exception with the given message."""
+    raise NameError(msg)
