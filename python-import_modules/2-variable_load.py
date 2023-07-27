1b66640b7d1172c305d81f847920b5ddb0d76ad8
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-07-27 18:13:23 +0300

    ex

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,5 +1,3 @@
-# Execute the contents of variable_load_2.py using exec()
-exec(variable_load_2_content)
-
-# Print the value of the variable 'a'
-print("The value of 'a' is:", a)
+if __name__ == "__main__":
+    exec(variable_load_2_content)
+    print("The value of 'a' is:", a)
