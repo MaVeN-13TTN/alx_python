848dce172657c16f86f1cb0fe83646849db053d0
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-07-27 22:14:40 +0300

    div

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,14 +1,15 @@
 if __name__ == "__main__":
-    def safe_print_division(a, b):
-        try:
-            result = a / b
-        except ZeroDivisionError:
-            result = None
-        finally:
-            #print("Inside result: {}".format(result))
-            result = safe_print_division(a, b)
-            print("{:d} / {:d} = {}".format(a, b, result))
+   def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
         return result
-
+a = 12
+b = 2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
 
 
