877082a70bbe98fb7dd58e9b8d5e5be91a016c1a
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-07-27 17:44:06 +0300

    argv

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,2 +1,14 @@
 import sys
-dir(sys)
+
+def test_arguments():
+    num_arguments = len(sys.argv) - 1 
+
+    if num_arguments == 0:
+        print(":")
+    else:
+        separator = ":"
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print(f"{i}{separator} {arg}")
+
+if __name__ == "__main__":
+    test_arguments()
