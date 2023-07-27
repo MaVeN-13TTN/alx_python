6143d5938390c807e2205d241004c6fa8d04c621
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-07-27 22:35:52 +0300

    err msg

---
+++ python-import_modules/5-raise_exception_msg.py
@@ -0,0 +1,6 @@
+
+def raise_exception_msg(message=""):
+    error_message = f"NameError: {message}"
+    exec("raise " + error_message)
+    def raise_exception_msg_with_line_number(message="", line_number=0):
+        error_message = f"NameError: {message} at line {line_number}"
