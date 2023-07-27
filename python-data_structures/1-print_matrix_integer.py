a9bb5afa43a3b8ba4dc323e6772e25043eb22dac
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-07-27 23:18:22 +0300

    matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,7 +1,15 @@
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for i, num in enumerate(row):
-            if i == len(row) - 1:
-                print("{:d}".format(num))
-            else:
-                print("{:d}".format(num), end=" ")
+    """Prints a matrix of integers.
+    Args:
+        matrix: A list of lists of integers.
+    Returns:
+        None.
+    """
+    # Check if the matrix is empty.
+    if not matrix:
+        return
+    # Print the first row of the matrix.
+    print(" ".join(str(i) for i in matrix[0]))
+    # Print the remaining rows of the matrix.
+    for row in matrix[1:]:
+        print(" ".join(str(i) for i in row))
