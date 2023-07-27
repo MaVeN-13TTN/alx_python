29f06a54c92c2d024d1b86c05206fbd92fec38c4
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-07-27 21:42:41 +0300

    argv

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,16 +1,17 @@
 import sys
 
-def test_arguments():
-    num_arguments = len(sys.argv) - 1 
+def print_arguments():
+    num_arguments = len(sys.argv) - 1
 
-    if num_arguments == 1:
-        print("{} argument".format(num_arguments))
-    elif num_arguments >1:
-        print("{} arguments".format(num_arguments))
+    if num_arguments == 0:
+        print("0 arguments.")
+    elif num_arguments == 1:
+        print("1 argument:")
+        print(f"1: {sys.argv[1]}")
     else:
-        separator = ":"
+        print(f"{num_arguments} arguments:")
         for i, arg in enumerate(sys.argv[1:], start=1):
-            print(f"{i}{separator} {arg}")
+            print(f"{i}: {arg}")
 
 if __name__ == "__main__":
-    test_arguments()
+    print_arguments()
