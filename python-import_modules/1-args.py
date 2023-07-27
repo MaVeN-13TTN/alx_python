cc58472b402002b90bbf0d0e33e1dcb46b5a1e1d
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-07-27 17:52:33 +0300

    arg

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -3,8 +3,10 @@ import sys
 def test_arguments():
     num_arguments = len(sys.argv) - 1 
 
-    if num_arguments == 0:
-        print(":")
+    if num_arguments == 1:
+        print("{} argument".format(num_arguments))
+    elif num_arguments >1:
+        print("{} arguments".format(num_arguments))
     else:
         separator = ":"
         for i, arg in enumerate(sys.argv[1:], start=1):
