c81145a2be91ac097f131b52b4fe66c9af913097
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-07-27 22:32:49 +0300

    err

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,7 +1,7 @@
 if __name__ == "__main__":
     def raise_exception():
-        raise TypeError("An exception has been raised")
-    try:
-        raise_exception()
-    except TypeError as te:
-        print("Exception has been raised")
+        #raise TypeError("An exception has been raised")
+        try:
+            raise_exception()
+        except TypeError as te:
+             print("Exception has been raised")
