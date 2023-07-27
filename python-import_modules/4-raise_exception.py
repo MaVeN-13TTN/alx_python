1b6da0f359399338ba54ad39f1cae97d20385eea
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-07-27 23:01:09 +0300

    exp

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,2 +1,8 @@
 def raise_exception():
-  raise Exception()
+    # Raise a TypeError exception with a custom message
+    raise TypeError("Custom exception message")
+
+try:
+    raise_exception()
+except TypeError as te:
+    print("Exception raised")
