1d9c3e7fe84d662eea7ef1d8e99a0ec451b954e6
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-08-02 22:44:49 +0300

    mat sqrd

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
-def square_matrix_simple(matrix=[]):
+def square_matrix_simple(matrix):
     square_matrix = list(map(lambda row: list(map(lambda num: num**2, row)), matrix))
     return square_matrix
 
-matrix=[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-new_matrix = square_matrix_simple()
+new_matrix = square_matrix_simple(matrix)
 print(new_matrix)
 print(matrix)
