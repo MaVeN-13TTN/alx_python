911f4c0a30431b56fd0c737c78b24334b7b03949
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-08-02 22:03:01 +0300

    sqr-matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,8 @@
-def square_matrix_simple(matrix=[]):
-    return [[num ** 2 for num in row] for row in matrix]
+def square_values(matrix):
+    # Use map() and lambda to compute the square of all integers in the matrix
+    square_matrix = list(map(lambda row: list(map(lambda num: num**2, row)), matrix))
+    return square_matrix
+
+result_matrix = square_values()
+print(result_matrix)
+
