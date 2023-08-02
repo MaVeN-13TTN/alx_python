fea0993ef450ae9b38eaf33200b3bce4731f4a58
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-08-02 23:19:13 +0300

    matrix integers

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -5,8 +5,8 @@ def print_matrix_integer(matrix=[[]]):
         for row in matrix:
             for i, num in enumerate(row):
                 if i == len(row) - 1:
-                    print("{:d}".format(num))
+                    print("{:4d}".format(num))
                 else:
-                    print("{:d}".format(num), end=" ")
+                    print("{:4d}".format(num), end=" ")
         
         
