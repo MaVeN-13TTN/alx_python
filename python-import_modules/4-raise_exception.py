0fb159238ae0c9cf9f7338a601cb243d6bc51a7a
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-08-02 23:36:45 +0300

    error

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,7 +1,6 @@
 def raise_exception():
-    raise TypeError("This function raises a TypeError exception.")
-
-try:
-    raise_exception()
-except TypeError as te:
-    print("Exception raised")
+    try:
+        # Raise the TypeError here
+        raise TypeError("This is a type exception!")
+    except TypeError as e:
+        print("Exception has been raised")
