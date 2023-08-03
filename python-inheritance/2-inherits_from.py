9e7f4d6f63a236338fc1e5fd4324ccd269e75864
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-08-04 00:11:49 +0300

    inherits

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,7 +1,9 @@
 """Defines an inherited class-checking function."""
 
+
 def inherits_from(obj, a_class):
     """Checks if an object is an inherited instance of a class.
+
     Args:
         obj (any): The object to check.
         a_class (type): The class to match the type of obj to.
