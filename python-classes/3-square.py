12c41c9668ebcdd2b99fa8c835b24f08b4b116f5
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-08-03 21:23:01 +0300

    sqr-3

---
+++ python-classes/3-square.py
@@ -0,0 +1,36 @@
+""" Square Class:defines a square by : (based on 2-square.py)
+"""
+
+
+class Square:
+    """__init__
+
+    The __init__ method initializes the size value of the square.
+
+    Attributes:
+        size (:obj:`int`, optional): The size of the square.
+
+    Raises:
+        TypeError: If `size` type is not `int`.
+
+        ValueError: If `size` is less than `0`.
+
+    """
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self._size = value
+
+    def area(self):
+        return self.size ** 2
