627c1b0d36ae7d42d67edb6c1a4906eb520573de
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-08-03 23:28:36 +0300

    geo-3

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,16 @@
+"""Integer validator"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+
+    def area(self):
+        """Area method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Integer validator method"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
