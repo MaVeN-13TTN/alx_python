4bc199ccf7fad634e954253da778024d16c3f6c9
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-08-03 23:25:48 +0300

    geo-2

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,9 @@
+"""Improve Geometry"""
+
+
+class BaseGeometry:
+    """Public instance method"""
+
+    def area(self):
+        """raises an Exception"""
+        raise Exception("area() is not implemented")
