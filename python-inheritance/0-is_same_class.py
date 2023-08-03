81c053b44a85482b3f3dc6fdb2f2037fb81e4d84
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-08-03 23:01:10 +0300

    exact

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,8 +1,12 @@
-def is_same_class(obj, a_class):
-    # Get the type of the object 'obj'
-    obj_type = type(obj)
-    
-    # Compare the type of the object with the specified class 'a_class'
-    # and return True if they are the same, otherwise return False
-    return obj_type == a_class
+"""module"""
 
+
+def is_same_class(obj, a_class):
+    """
+    Returns True if the object is exactly an instance of the specified class
+    otherwise False
+    """
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
