d4b0561ddb3a25e51a6d073ddb6aa966f3fff3d5
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-08-09 18:01:25 +0300

    validate attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -33,6 +33,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        """width setter"""
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -46,6 +47,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        """height setter"""
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -59,6 +61,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        """x setter"""
         if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -72,6 +75,7 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        """y setter"""
         if type(value) != int:
             raise TypeError("y must be an integer")
         if value < 0:
