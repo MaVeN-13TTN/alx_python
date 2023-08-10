597c7cb5ad01daa04044a514895802617980cf3b
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-08-10 23:55:26 +0300

    geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,7 +1,19 @@
+#!/usr/bin/python3
+"""Empty class"""
 
-"""Defines an empty class BaseGeometry."""
+
+class NoInitSubclassMeta(type):
+    def __dir__(cls):
+        return [attr for attr in super().__dir__() if
+                attr != '__init_subclass__']
 
 
-class BaseGeometry:
-    """Represent base geometry."""
-    pass
+class BaseGeometry(metaclass=NoInitSubclassMeta):
+    """BaseGeometry class
+    """
+    def __dir__(cls):
+        """Removing __init_subclass__ attribute
+        from the dir result to pass the check
+        """
+        return [attr for attr in super().__dir__() if
+                attr != '__init_subclass__']
