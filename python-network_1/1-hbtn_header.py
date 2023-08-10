197eab9c16025bcdea6b36b5c2294941d07b3aa5
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-08-10 22:44:32 +0300

    response header value

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,14 +1,14 @@
-"""takes in a URL, sends a request to the URL and displays the
-value of the X-Request-Id variable found in the header of the response.
-
-Usage: ./1-hbtn_header.py <URL>
+"""A script that:
+- takes in a URL,
+- sends a request to the URL and displays the value
+- of the X-Request-Id variable found in the header ofthe response.
 """
-# Import the requests module 
-import requests
 import sys
-url = str(sys.argv[1])
-response = requests.get('http://' + url) # Send GET Request
-print ("X-Request-ID;", response.headers['x-requestid'])
+import urllib.request
 
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
