95a8aedada8ea6ba7cd69b7ad0f07081b25fd94e
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-08-10 23:38:04 +0300

    error code

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,19 +1,8 @@
+import requests
 import sys
-from urllib import requests
-
-def main():
-    # Get the URL from command-line argument
-    url = sys.argv[1]
-    
-    # Send a GET request to the provided URL
-    response = requests.get(url)
-    
-    # Display the body of the response
+url = sys.argv[1]
+response = requests.get(url)
+if response.status_code >= 400:
+    print("Error code:", response.status_code)
+else:
     print(response.text)
-    
-    # Check if the status code is greater than or equal to 400
-    if response.status_code >= 400:
-        print("Error code:", response.status_code)
-
-if __name__ == "__main__":
-    main()
