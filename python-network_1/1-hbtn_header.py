d5c478f97cec76fba4ac1a224a0a2fc1df503fc9
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-08-10 22:39:54 +0300

    requests

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,28 +3,12 @@ value of the X-Request-Id variable found in the header of the response.
 
 Usage: ./1-hbtn_header.py <URL>
 """
-# Import the requests module
-from urllib import requests
+# Import the requests module 
+import requests
+import sys
+url = str(sys.argv[1])
+response = requests.get('http://' + url) # Send GET Request
+print ("X-Request-ID;", response.headers['x-requestid'])
 
-# Define the URL to fetch
-url = 'https://alu-intranet.hbtn.io/status'
 
-# Send a GET request to the URL
-response = requests.get(url)
 
-# Check if the response status code is 200 (OK)
-if response.status_code == 200:
-    # Parse the JSON data from the response
-    data = response.json()
-    
-    # Display the header for the response body
-    print("Body response:")
-    
-    # Display the type of the data
-    print("\t- type:", type(data))
-    
-    # Display the content of the data
-    print("\t- content:", data)
-else:
-    # Display an error message along with the status code
-    print("Error:", response.status_code)
