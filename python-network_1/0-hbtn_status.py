822fda0385790e28f197611727a5988809057b5f
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-08-10 23:49:04 +0300

    requests

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,7 +2,7 @@
 Python script that fetches https://alu-intranet.hbtn.io/status. 
 The script uses the requests package to make the request and then prints the body of the response.
 '''
-from urllib import requests
+import requests
 
 if __name__ == "__main__":
     # Define the URL to fetch
