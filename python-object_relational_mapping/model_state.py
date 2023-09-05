fa9008c0a41de18beb9729088c90eed3f2716358
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-09-05 16:48:41 +0300

    python file that contains the class definition of a State and an instance Base = declarative_base()

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,10 +1,34 @@
-'''
-    python file that contains the class definition of a State and an instance Base = declarative_base()
-'''
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
 Base = declarative_base()
+
 class State(Base):
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"State(id={self.id}, name='{self.name}')"
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    # Replace 'your_username', 'your_password', and 'your_database' with your MySQL credentials.
+    DATABASE_URL = 'mysql://your_username:your_password@localhost:3306/your_database'
+
+    # Create the database engine and session
+    engine = create_engine(DATABASE_URL, echo=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Create the tables in the database
+    Base.metadata.create_all(engine)
+
+    # Close the session
+    session.close()
