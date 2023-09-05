d29e357c64f06c23cd8c86ec7945b6f74dc979d7
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-09-05 16:49:39 +0300

    class definition of a State and an instance Base = declarative_base()

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,6 +4,21 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class State(Base):
+    """
+    Represents a state in a database table.
+
+    Attributes:
+        id (int): An auto-generated, unique integer representing the state's ID. It is also the primary key.
+        name (str): A string representing the name of the state. It can't be null and has a maximum length of 128 characters.
+
+    Methods:
+        __init__(self, name):
+            Initializes a new State instance with the provided name.
+
+        __str__(self):
+            Returns a string representation of the State object in the format "State(id=<id>, name='<name>')".
+
+    """
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
