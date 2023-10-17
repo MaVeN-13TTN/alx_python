9410cdbdeef3e8062145e0bd5411c42aed753535
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-10-17 16:55:58 +0300

    gathering data

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,35 +1,26 @@
 import requests
 import sys
 
-def get_todo(id: int):
+def get_todo(id:int):
     url = f'https://jsonplaceholder.typicode.com/users/{id}'
     url_2 = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
-    
-    try:
-        x = requests.get(url)
-        x.raise_for_status()  # Check for HTTP request errors
-        json_data = x.json()
-        EMPLOYEE_NAME = json_data['name']
-        
-        y = requests.get(url_2)
-        y.raise_for_status()  # Check for HTTP request errors
-        json_data_2 = y.json()
-        
-        TOTAL_NUMBER_OF_TASKS = len(json_data_2)
-        NUMBER_OF_DONE_TASKS = 0
-        
-        for i in json_data_2:
-            if i['completed'] == True:
-                NUMBER_OF_DONE_TASKS += 1
-        
-        print(f'Employee {EMPLOYEE_NAME} is done with tasks ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
-        for i in json_data_2:
-            if i['completed'] == True:
-                print(f"\t {i['title']}")
-
-    except requests.exceptions.RequestException as e:
-        print("An error occurred while making the HTTP request:")
-        print(e)
+    x = requests.get(url)
+    json_data = x.json()
+    EMPLOYEE_NAME = json_data['name']
+    # print(json_data)
+    y = requests.get(url_2)
+    json_data_2 = y.json()
+    TOTAL_NUMBER_OF_TASKS = len(json_data_2)
+    NUMBER_OF_DONE_TASKS=0
+    for i in json_data_2:
+        if i['completed']==True:
+            NUMBER_OF_DONE_TASKS+=1
+    print(f'Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
+    for i in json_data_2:
+        if i['completed']==True:
+            print(f"\t {i['title']}")
+    __doc__="""doc for class"""
+__doc__="""doc for module"""
 
 if __name__ == "__main":
     if len(sys.argv) != 2:
