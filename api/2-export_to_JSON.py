60718a9634bea71c9189d02f0608db40cae7b256
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-10-17 18:04:21 +0300

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,36 +1,49 @@
-import sys
+"""
+This script uses an API to retrieve employee task information
+and display in a special format.
+
+It retrieves employees name, task completed with their titles.
+"""
+import json
 import requests
-import csv
+import sys
 
-def gather_data(employee_id):
-    # Fetch employee details
-    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    employee_response = requests.get(employee_url)
-    employee_data = employee_response.json()
-    employee_name = employee_data.get("username")
-
-    # Fetch employee's TODO list
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-    todos_response = requests.get(todos_url)
-    todos_data = todos_response.json()
-
-    # Export data to CSV file
-    filename = f"{employee_id}.csv"
-    with open(filename, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for task in todos_data:
-            task_id = task.get("id")
-            task_title = task.get("title")
-            task_completed = task.get("completed")
-            writer.writerow([employee_id, employee_name, task_completed, task_title])
-
-    print(f"Data exported to {filename} successfully.")
-
+# No execution of this file when imported
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 export_to_CSV.py [employee_id]")
-        sys.exit(1)
 
-    employee_id = int(sys.argv[1])
-    gather_data(employee_id)
+    # Pass employee id on command line
+    id = sys.argv[1]
+
+# APIs
+    userTodoURL = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+        id)
+    userProfile = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+
+# Make requests on APIs
+    todoResponse = requests.get(userTodoURL)
+    profileResponse = requests.get(userProfile)
+
+# Parse responses and store in variables
+    todoJson_Data = todoResponse.json()
+    profileJson_Data = profileResponse.json()
+
+# Get employee information
+    employeeName = profileJson_Data['username']
+
+    dataList = []  # Empty list to store the dictionaries
+
+    for data in todoJson_Data:
+        dataDict = {
+            "task": data['title'], "completed": data['completed'], "username": employeeName}
+        dataList.append(dataDict)
+
+# A dictionary of list of dictionaries to be exported to JSON
+    outputData = {profileJson_Data['id']: dataList}
+
+# Specify the JSON file path
+    json_file_path = '{}.json'.format(todoJson_Data[0]['userId'])
+
+# Open the JSON file in write mode
+    with open(json_file_path, 'w') as json_file:
+        # Serialize and write the data to the JSON file
+        json.dump(outputData, json_file)
