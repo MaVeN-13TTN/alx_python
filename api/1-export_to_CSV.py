7f89ac02c48114f3ee667dfbac8829260987f6fa
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-10-17 17:10:40 +0300

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,16 +1,32 @@
-import requests,sys,csv
+import requests,sys
 
-
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
-
-    with open("{}.csv".format(user_id), "w", newline="\n") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-        ) for t in todos]
+def get_todo(id:int):
+    url = f'https://jsonplaceholder.typicode.com/users/{id}'
+    url_2 = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
+    x = requests.get(url)
+    json_data = x.json()
+    EMPLOYEE_NAME = json_data['name']
+    # print(json_data)
+    y = requests.get(url_2)
+    json_data_2 = y.json()
+    TOTAL_NUMBER_OF_TASKS = len(json_data_2)
+    NUMBER_OF_DONE_TASKS=0
+    for i in json_data_2:
+        if i['completed']==True:
+            NUMBER_OF_DONE_TASKS+=1
+    print(f'Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
+    for i in json_data_2:
+        if i['completed']==True:
+            print(f"\t {i['title']}")
+    __doc__="""doc for class"""
 __doc__="""doc for module"""
+
+    # Export data to CSV file
+with open(f'{id}.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        for task in json_data_2:
+            writer.writerow([id, EMPLOYEE_NAME, task['completed'], task['title']])
+if __name__ == "__main__":
+    id = int(sys.argv[1])
+    get_todo(id)
