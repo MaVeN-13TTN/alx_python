d115b69dc510902782c2246045afaf494cd6c652
Sam-13TTN <117756622+Sam-13TTN@users.noreply.github.com>
2023-10-17 17:13:31 +0300

    gathering data

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,25 +1,37 @@
-import requests,sys
+#!/usr/bin/python3
+"""
+Script that, using this REST API, for a given employee ID, returns
+information about his/her TODO list progress
+"""
 
-def get_todo(id:int):
-    url = f'https://jsonplaceholder.typicode.com/users/{id}'
-    url_2 = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
-    x = requests.get(url)
-    json_data = x.json()
-    EMPLOYEE_NAME = json_data['name']
-    # print(json_data)
-    y = requests.get(url_2)
-    json_data_2 = y.json()
-    TOTAL_NUMBER_OF_TASKS = len(json_data_2)
-    NUMBER_OF_DONE_TASKS=0
-    for i in json_data_2:
-        if i['completed']==True:
-            NUMBER_OF_DONE_TASKS+=1
-    print(f'Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
-    for i in json_data_2:
-        if i['completed']==True:
-            print(f"\t {i['title']}")
-    __doc__="""doc for class"""
-__doc__="""doc for module"""
+import json
+import requests
+from sys import argv
+
+
 if __name__ == "__main__":
-    id = int(sys.argv[1])
-    get_todo(id)
+
+    sessionReq = requests.Session()
+
+    idEmp = argv[1]
+    idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
+    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
+
+    employee = sessionReq.get(idURL)
+    employeeName = sessionReq.get(nameURL)
+
+    json_req = employee.json()
+    name = employeeName.json()['name']
+
+    totalTasks = 0
+
+    for done_tasks in json_req:
+        if done_tasks['completed']:
+            totalTasks += 1
+
+    print("Employee {} is done with tasks({}/{}):".
+          format(name, totalTasks, len(json_req)))
+
+    for done_tasks in json_req:
+        if done_tasks['completed']:
+            print("\t " + done_tasks.get('title'))
